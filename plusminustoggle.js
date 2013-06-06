beac59a3565228d168e0ca632e69127c0353f1f7
Heath Robbins <heathrob89@gmail.com>
2013-06-06 02:05:57 -0700

    elegant js

--- plusminustoggle.js
+++ plusminustoggle.js
@@ -1,28 +1,22 @@
-//suck mah dick
+//CONSTANTS
 var NUM_LIST_ITEMS = 4;
+var PLUSMINUS = ["url('assets/images/minus.png')", "url('assets/images/plus.png')"];
 
-			var plusminus = new Array();
-			plusminus[0] = "url('minus.png')";
-			plusminus[1] = "url('plus.png')";
+//keep track of toggle expandedState
+var expandedState = new Array(NUM_LIST_ITEMS);
 
-			//keep track of toggle state
-			var state = new Array(NUM_LIST_ITEMS);
+$(document).ready(function(){
+	for(var i = 0; i < NUM_LIST_ITEMS; i++){
+		expandedState[i] = 1;
 
-			for(var i = 0; i < state.length; i++){
-				state[i] = 1;
-			}
-
-			$(document).ready(function(){
-				for(var i = 0; i < NUM_LIST_ITEMS; i++){
-					$("#plus" + (i+1)).click(
-						function(index){
-							return function(){
-								$("#block" + (index+1)).fadeToggle("slow", "linear");
-								state[index] = (state[index]+1)%(plusminus.length);
-								$(this).css("background-image", plusminus[state[index]]);
-								console.log("state: " + state[index]);
-							};
-						}(i)
-					);
-				}
-			}); 
+		$("#plus" + (i+1)).click(
+			function(index){
+				return function(){
+					$("#block" + (index+1)).fadeToggle("slow", "linear");
+					expandedState[index] = (expandedState[index]+1)%(PLUSMINUS.length);
+					$(this).css("background-image", PLUSMINUS[expandedState[index]]);
+				};
+			}(i)
+		);
+	}
+}); 
